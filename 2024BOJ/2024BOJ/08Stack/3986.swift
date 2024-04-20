89f4f9f686372682c378a0f1cccd997612dc5a16
dbqls200 <dbqls200@gmail.com>
2024-04-20 18:44:31 +0900

    3986 edit

--- 2024BOJ/2024BOJ/08Stack/3986.swift
+++ 2024BOJ/2024BOJ/08Stack/3986.swift
@@ -3,6 +3,31 @@
 //  2024BOJ
 //
 //  Created by 김유빈 on 4/20/24.
-//
+//  좋은 단어
+//  https://www.acmicpc.net/problem/3986
 
 import Foundation
+
+func _3986() {
+    var n: Int = Int(readLine()!)!
+
+    for _ in 0..<n {
+        let arr = readLine()!
+        var tmp: [Character] = []
+        
+        for s in arr {
+            let t = tmp.last
+            if s == t {
+                tmp.popLast()
+            } else {
+                tmp.append(s)
+            }
+        }
+        
+        if !tmp.isEmpty {
+            n -= 1
+        }
+    }
+    
+    print(n)
+}
